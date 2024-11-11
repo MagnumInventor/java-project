350c7f0becfadd4b1b62903764130755e8086013
Олександр Маркович <markoleks08@gmail.com>
2024-11-11 20:37:50 +0200

    Update main.java
    import list

--- main.java
+++ main.java
@@ -1,7 +1,15 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
-        String[] GerNounWords = { "heute", };
+        ArrayList<String> GerNounsList = new ArrayList<>();
+
+        // Додаємо слова до списку
+        GerNounsList.add("apple");
+        GerNounsList.add("banana");
+        GerNounsList.add("cherry");
+        GerNounsList.add("date");
 
     }
 }
